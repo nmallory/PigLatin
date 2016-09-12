cdd5729b9868c12170494dfb77a8ecb2c7a53760
Nicole Mallory <nmall76@hotmail.com>
2016-09-12 16:53:26 -0400

    Initial commit

---
+++ src/PigLatin.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PigLatin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		String words;
+		
+		System.out.println("Welcome to the Pig Latin Translator!");
+
+		
+		//Create scanner
+		Scanner input = new Scanner(System.in);
+		
+		//Prompt the user to enter a line to be translated.
+		System.out.println("Enter a line to be translated: this sentence exists here: ");
+		words = input.next();
+		
+		
+	}
+
+}
