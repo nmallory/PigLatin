c8b6246f014e058aa50c856900e9aa0d9e231f91
Nicole Mallory <nmall76@hotmail.com>
2016-09-16 09:45:39 -0400

    Final version

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,46 +1,56 @@
 import java.util.Scanner;
 
 public class PigLatin {
-	
-	//Method to check for vowels
-			public static void AVowel(){
-			
-			String words = "";
-			//char[] vowelArray = {'a', 'e', 'i', 'o', 'u'};
-			if(words.startsWith("a") || words.startsWith("e")|| words.startsWith("i")|| words.startsWith("o")|| words.startsWith("u")){
-				}else{
-				
-								
-			}
-		}
 
 	public static void main(String[] args) {
 
-		String words;
-		String yes = "";
-		
+		String words = "";
+		String yes = "yes";
+
 		System.out.println("Welcome to the Pig Latin Translator!");
-		
-		do{
-		//Create scanner
+
+		// Create scanner
 		Scanner input = new Scanner(System.in);
-		
-		//Prompt the user to enter a line to be translated.
-		System.out.println("Enter a line to be translated:");
-		words = input.next();
-		System.out.println(words.toLowerCase());
-		words = words.concat("way");
-		System.out.println(words.toLowerCase());
-		
-		//Ask user if they want to translate another line
-		System.out.println("Translate another line? (y/n): ");
+
+		do {
+			// Prompt the user to enter a line to be translated.
+			System.out.println("Enter a line to be translated:");
+			words = input.next();
+			words = words.toLowerCase();
+			
+			//If statement to check if word starts with a vowel
+			if (words.startsWith("a") || words.startsWith("e") || words.startsWith("i") || words.startsWith("o")
+					|| words.startsWith("u")) {
+				
+			//Concating "way" to end of word entered
+				words += "way";
+
+			}
+			for (int i = 0; i < words.length(); i++) {
+				// Checks to see if first letter is not a vowel
+					if (!words.startsWith("a") && !words.startsWith("e") && !words.startsWith("i")
+							&& !words.startsWith("o") && !words.startsWith("u")) {
+
+						//CharAt returns first character at a specified index
+						char firstChar = words.charAt(0);
+						
+						//Removes first letter of word entered
+						words = words.substring(1);
+						
+						words = words.substring(0) + firstChar +"ay" ;
 	
-			yes = input.nextLine();	
-			yes = input.nextLine();
-		}
-			while(yes.equalsIgnoreCase ("y"));
+					} // ends loop for if statement for consonants
+						
+			} // forloop
+			System.out.println(words);
+			
+			//Ask user if they want to translate another word
+			System.out.println("Translate another line? (yes/no): ");
+
+			input.nextLine();
+			//Clears out scanner
+			input.nextLine();
+			
+		} while (yes.equalsIgnoreCase("yes"));
 	}
-	
-}
-
-
+}// class
